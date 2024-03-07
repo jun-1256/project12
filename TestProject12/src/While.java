bac5617b87635f9a3fa66cc38efec69e69942eab
jun-1256 <juns_1201@icloud.com>
2024-03-07 20:35:18 +0900

    2024/03/07

---
+++ TestProject12/src/While.java
@@ -0,0 +1,35 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		// while文の基礎
+		//	初期値; 
+		//	while(条件式) {
+		// 		処理内容; 増減式;
+		//	}
+		
+		// (例)
+		//	int i = 1;
+		//	while ( i <= 5 ) {
+		//		System.out.println(i);
+		//		i++;
+		//	}
+		
+		int i = 0;
+		while (i < 10){
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("\n");
+		
+		int a = 5;
+		while (a < 10) {
+			System.out.println(a);
+			a++;
+		}
+		
+
+	}
+
+}
---
+++ TestProject12/src/While3.java
@@ -0,0 +1,43 @@
+
+public class While3 {
+
+	public static void main(String[] args) {
+		// while文の基礎
+		
+		// 初期値が1で、2ずつ増える場合
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i+=2;
+		}
+		
+		System.out.println("\n");
+		
+		// 初期値が1で、3ずつ増える場合
+		int a = 1;
+		while (a <= 10) {
+			System.out.println(a);
+			a+=3;
+		}
+		
+		System.out.println("\n");
+		
+		// 初期値が10で、2ずつ減る場合
+		int b = 10;
+		while (b >= 1) {
+			System.out.println(b);
+			b-=2;
+		}
+		
+		System.out.println("\n");
+		
+		// 初期値が10で、3ずつ減る場合
+		int c = 10;
+		while (c >= 1) {
+			System.out.println(c);
+			c-=3;
+		}
+
+	}
+
+}
